77056d739c5162eda1b6011eb27594394117f264
SamySabry <samyelsheref@gmail.com>
2024-12-15 00:11:25 +0200

    Add project files.

---
+++ Context/LogContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DominModels.Context
+{
+    public class LogContext: DbContext
+    {
+        public LogContext(DbContextOptions<LogContext> options)
+    : base(options)
+        {
+        }
+
+        public virtual DbSet<Log> Logs { get; set; }
+
+    }
+}
